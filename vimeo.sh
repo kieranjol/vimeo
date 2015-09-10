4e7c9ce12ae4c636e556f24a011f34494f2420aa
Kieran O'Leary <kieranoleary@Kierans-iMac.local>
2015-09-10 01:15:25 +0100

    Timecode improvements
    Timecode position and size is a little more variable now.
    H264 quality hardcoded as crf 19
    Timecode source is grabbed from either format or stream ffprobe

--- vimeo.sh
+++ vimeo.sh
@@ -1,12 +1,68 @@
 #!/bin/bash -x
 
+
+
+
+echo "do you just want an SD bitc h264 Say Y or N?"
+read bitc
+if [[ "${bitc}" == "Y" || "${bitc}" == "y" ]] ; then
+
+	eval $(ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height "$1")
+	size=${streams_stream_0_height}
+	echo $size
+	
+	if [[ "${size}" == "576" ]] ; then
+		textoptions=("fontsize=45:x=360-text_w/2:y=480")
+	elif [[ "${size}" == "486" || "${size}" == "480" ]] ; then
+		textoptions=("fontsize=45:x=360-text_w/2:y=400")
+	elif [[ "${size}" == "1080" || "${size}" == "1080" ]] ; then
+		textoptions=("fontsize=90:x=957-text_w/2:y=960")
+	fi
+	
+	
+	
+	
+
+
+framerate=($(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=noprint_wrappers=1:nokey=1 "$1"))
+tctest=($(ffprobe -v error -select_streams v:0 -show_entries stream_tags=timecode -of default=noprint_wrappers=1:nokey=1 "$1"))
+if [[ "${tctest}" == "" ]] ; then
+	IFS=: read -a timecode < <(ffprobe -v error -show_entries format_tags=timecode -of default=noprint_wrappers=1:nokey=1 "$1")
+else 
+	IFS=: read -a timecode < <(ffprobe -v error -show_entries stream_tags=timecode -of default=noprint_wrappers=1:nokey=1 "$1")
+fi
+		printf -v timecode "'%s\:%s\:%s\:%s'" "${timecode[@]}"
+		echo "$timecode"
+
+		drawtext_options=(
+		    fontsize=45
+		    fontfile="/Library/Fonts/Arial Black.ttf"
+		    fontcolor=white
+		    timecode="$timecode"
+		    rate="$framerate"
+		    boxcolor=0x000000AA
+		    box=1
+			$textoptions
+		    #x=360-text_w/2
+		    #y=480
+		)
+
+		drawtext_options=$(IFS=:; echo "${drawtext_options[*]}")
+		ffmpeg -i "$1" -c:v libx264 -crf 19 -pix_fmt yuv420p -vf \
+		    drawtext="$drawtext_options" \
+		    "${1}_BITC.mov"
+		
+		
+else
+		
+
 echo "codec? Choose things like h264, prores etc"
 read "codec";
 
 container=("mov")
 
 if [[ "${codec}" == "h264" || "${codec}" == "H264" ]] ; then
-	videooptions=("-c:v h264 -pix_fmt yuv420p")
+	videooptions=("-c:v h264 -pix_fmt yuv420p -crf 19")
 elif [[ "${codec}" == "ffv1" || "${codec}" == "FFV1" ]] ; then
 	videooptions=("-c:v ffv1 -level 3 -g 1")
 else videooptions=("-c:v $codec")
@@ -28,7 +84,7 @@ echo "Any further alterations such as resizing or new container? Answer Y or N "
 read "extra"
 if [[ "${extra}" == "y" || "${extra}" == "Y" ]] ; then
 
-echo "resize?"
+echo "resize? Y or N"
 read "resize";
 if [[ "${resize}" == "y" || "${resize}" == "Y" ]] ; then
 	echo "enter desired resolution in the following format - 720:576 OR 1024:576 OR 1920:1080"
@@ -44,3 +100,4 @@ fi
 
 ffmpeg -i "${1}" -map 0 ${videooptions} ${filteroptions} ${audiooptions} -dn "${1}_vimeo.${container}"
 
+fi
