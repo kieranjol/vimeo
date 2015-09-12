57659bf8dce9999e1bb2356091bc61791324efa0
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-09-12 17:32:59 +0100

    Delete newvimeo.sh

--- newvimeo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-b='-show_entries format_tags=timecode'
-echo $b
-IFS=: read -a timecode < <(ffprobe -v error "${b[@]}" -of default=noprint_wrappers=1:nokey=1 "$1")
-echo "$timecode"
-
-		
