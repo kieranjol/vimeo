a62328ba0f76e0c62e7de21808b6de1b933b81a8
Kieran O'Leary <kieranoleary@Kierans-iMac.local>
2015-09-13 14:06:11 +0100

    cleanup

--- dvd.sh
+++ dvd.sh
@@ -1,5 +1,4 @@
-#!/bin/bash -x
-
+#!/bin/bash 
 
 sourcepath="$(dirname "$1")" 
 filename="$(basename "$1")"
@@ -8,7 +7,6 @@ filenoext="${filename%.*}"
 echo "Would you like to burn straight to DVD Y or N?"
 read "burn";
 
-	
 export VIDEO_FORMAT=PAL
 ffmpeg -i "$1" -target pal-dvd "$1"_dvd.mpg
 mkdir "$sourcepath/$filenoext"
@@ -19,8 +17,6 @@ mkisofs -dvd-video -V IFIARCHIVE -o "$1".iso "$sourcepath/$filenoext/dvd"
 
 if [[ "${burn}" == "Y" || "${burn}" == "y" ]] ; then
 	hdiutil burn "$1".iso
-	
 else
-	echo "done"
-	
+	exit
 fi
